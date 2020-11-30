1d5d99ab3827f7ca9872ea274fe96cd832eb6104
Matheus Mendes <matheus121968@hotmail.com>
2020-11-29 23:31:32 -0300

    oooo

--- condicionais.php
+++ condicionais.php
@@ -10,4 +10,25 @@ if ($numero == 10) {
 echo "<hr>";
 $media = 7;
 
-echo ($media >= 7) ? "aprovado!" : "reprovado!";// operador ternario para substituir o if/else
+echo ($media >= 7) ? "aprovado!"."<br>"."-------------"."<br>" : "reprovado!"."<br>"."-------------"."<br>";// operador ternario para substituir o if/else
+
+$cor = "vermelho";
+
+switch ($cor):
+
+    case "verde":
+        echo "A sua cor favorita e verde";
+    break;
+
+    case "vermelho":
+        echo "A sua cor favorita e vermelho";
+    break;
+
+    case "azul":
+        echo "A sua cor favorita e azul";
+    break;
+
+    default:
+    echo "Sua cor favoria nao e vermelho, verde ou azul";
+
+endswitch;
