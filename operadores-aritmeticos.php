4d3615d26f7ea115c06745e92cc5e3e776af426e
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2020-11-29 23:18:54 -0300

    operadores

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,46 @@
+<?php
+
+echo 10 + 10;
+echo '<br>';
+echo 10 - 5;
+echo '<br>';
+echo 3 * 5;
+echo '<br>';
+echo 15 / 5;
+echo '<br>';
+echo 15 % 5;
+echo '<br>';
+echo 3 ** 5;
+echo '<br>';
+
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+$e = 16;
+
+echo '<hr>';
+
+$adicao = $a + $b;
+echo $adicao;
+echo '<hr>';
+
+$subtracao = $c - $a;
+echo $subtracao;
+echo '<hr>';
+
+$multiplicacao = $d * $a;
+echo $multiplicacao;
+echo '<hr>';
+
+$divisao = $c / $a;
+echo $divisao;
+echo '<hr>';
+
+$modulo = $e % $d;
+echo $modulo;
+echo '<hr>';
+
+$expo = $a ** $d;
+echo $expo;
+echo '<hr>';
