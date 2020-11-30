d9c2975eb9f64fbebaae1eaa3f7b9aed6bd9a749
Matheus Mendes <matheus121968@hotmail.com>
2020-11-30 00:40:43 -0300

    repet

---
+++ operadores-de-comparacao.php
@@ -0,0 +1,61 @@
+<?php
+
+if(10 == 10):
+    echo "Positivo";
+else:
+    echo "negativo";
+
+    if (10 === "10") {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 != 11) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 !== 11) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 <> 11) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 < 9) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 > 7) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 >= 1) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 <= 11) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+    
+    if (10 <=> 20) {
+        echo "Positivo";
+    } else {
+        echo "negativo";
+    }
+endif;
