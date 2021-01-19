aeac86b9a8c4febb05f124e5a1a3a9dd2b7b89f3
Matheus Mendes <matheus121968@hotmail.com>
2021-01-18 21:36:49 -0300

    Update funcoes-para-strings.php
    up

--- funcoes-para-strings.php
+++ funcoes-para-strings.php
@@ -12,3 +12,21 @@ echo substr($mensagem, 4). "<br>";
 
 $obj = "mouse";
 echo str_pad($obj, 7, "**"). "<br>";
+
+$string = str_repeat("mouse ", 2);
+echo $string;
+
+echo "<hr>";
+
+$mensagem = "ola mundo";
+
+echo strlen ($mensagem);
+
+echo "<hr>";
+
+$text = "A selecao Argentina sera a campea da copa do mundo";
+
+$novotext = str_replace("Argentina", "Brasil", $text);
+echo $novotext;
+echo "<hr>";
+echo strpos($text, "copa");
