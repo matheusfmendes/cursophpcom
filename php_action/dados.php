43713b39cb0b0d875f87259b310b35abcd92f64d
Matheus Mendes <matheus121968@hotmail.com>
2021-05-17 22:25:03 -0300

    news
    aaaa

---
+++ php_action/dados.php
@@ -0,0 +1,10 @@
+<?php
+
+// $nome = $_POST['nome'];
+// $email = $_POST['email'];
+
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+
+echo "Seu nome e ".$nome."seu email e ".$email;
+// $enviar = $_GET['enviar'];
