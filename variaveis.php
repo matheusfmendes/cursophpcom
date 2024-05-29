f8a727060c45bc705d4c3a466165dbfd9959d394
Matheus Mendes <matheus121968@hotmail.com>
2024-05-29 16:38:09 -0300

    Update variaveis.php

--- variaveis.php
+++ variaveis.php
@@ -1,7 +1,14 @@
 <?php 
-$nome = 'Matheus Ferreira Mendes';
-$idade = 2020 - 1995;
-$altura = 1.80;
+echo "Digite o seu nome: \n";
+$nome =  (string) fgets(STDIN);
+echo "Digite o ano: \n";
+$ano_atual = (Double) fgets(STDIN);
+echo "Digite o ano do seu aniversario: \n";
+$ano_nascimento = (Double) fgets(STDIN);
+
+$idade = $ano_atual - $ano_nascimento;
+echo "Digite a sua autura: \n"
+$altura = (float) fgets(STDIN);;
 
 echo "Meu nome é ".$nome.",minha idade é ".$idade." e minha altura é ".$altura;
 ?>
